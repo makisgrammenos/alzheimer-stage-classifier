b5b905e17d1e269f259d34afb065b87ee5e4f1ce
makisgrammenos <makisgramm5@gmail.com>
2020-08-19 14:45:44 +0300

    update

--- train.py
+++ train.py
@@ -62,7 +62,6 @@ opt = keras.optimizers.Adam(learning_rate=0.001)
 model.compile(optimizer=opt, loss="sparse_categorical_crossentropy", metrics=["accuracy"], )
 
 
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 history = model.fit(train_data, train_labels, epochs=10, validation_data=(val_data, val_labels)
                     )
 
